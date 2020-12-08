bee0a99dbf8e62739f4e8e6bc51681685d0051c0
Peter Boyle <boyle.peter614@gmail.com>
2020-12-08 00:28:42 -0500

    DB, tables created, package.json updated with start and dependancy data

---
+++ emsDB.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS emsDB;
+
+CREATE DATABASE emsDB;
+
+USE emsDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10,2) NULL,
+  deptId INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  roleId INT NULL,
+  mgrId INT NULL,
+  PRIMARY KEY (id)
+);
